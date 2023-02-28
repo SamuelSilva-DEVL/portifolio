d9e7e3ac58374a2b2646d5765f05afff0eacf158
Samuel Silva <samuelmec1@gmail.com>
2023-02-28 16:47:16 -0300

    Adicionando menuMobile e fazendo validação para abrir apenas em certa resolução

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,13 +2,16 @@ import { Header } from "../../componentes/header";
 import { useTheme } from "styled-components";
 import { Footer, Title, Wrapper} 
 from "./styles";
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { Main } from "../../componentes/Main";
+import { CardProjeto } from "../../componentes/CardProjeto";
+import { MenuMobile } from "../../componentes/MenuMobile";
 
 export function HomePage(){
   const {colors} = useTheme()
+  const [menuIsVisible, setMenuIsVisible] = useState(false)
 
   useEffect(() => {
     AOS.init({
@@ -19,11 +22,16 @@ export function HomePage(){
 
   return(
     <Wrapper>
-      <Header />
+      <MenuMobile 
+        menuIsVisible={menuIsVisible}
+        setMenuIsVisible={setMenuIsVisible}
+      />
+      <Header setMenuIsVisible={setMenuIsVisible}/>
       <Main />
       <Footer>
-        <Title size="20px">Copyright Samuel Silva 2023</Title>
+        <Title size="20px">Copyright &copy; Samuel Silva 2023</Title>
       </Footer>
+      <CardProjeto />
     </Wrapper>
   )
 }
